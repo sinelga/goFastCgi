f81c41a8cd26f4f398ec831b9063ad55e8639f7b
Sinelga <support@sinelga.com>
2013-10-08 22:57:55 +0300

    prod

--- goFastCgi/cleanupdb_example.bash
+++ goFastCgi/cleanupdb_example.bash
@@ -1,3 +1,6 @@
 sqlite3 singo.db 'delete from keywords where locale="it_IT" and Themes="finance"'
+sqlite3 singo.db 'delete from phrases where locale="it_IT" and Themes="finance"'
 sqlite3 singo.db 'delete from keywords where locale="fi_FI" and Themes="finance"'
+sqlite3 singo.db 'delete from phrases where locale="fi_FI" and Themes="finance"'
 sqlite3 singo.db 'delete from keywords where locale="fi_FI" and Themes="porno"'
+sqlite3 singo.db 'delete from phrases where locale="fi_FI" and Themes="porno"'
--- goFastCgi/usefull.bash
+++ goFastCgi/usefull.bash
@@ -16,6 +16,11 @@ sqlite3 singo.db 'delete from keywords where locale="it_IT" and Themes="finance"
 
 sqlite3 singo.db 'insert into keywords ( Locale,Themes,Keyword) select Locale,Themes,Keyword from it_IT_finance_keywords where Block=0'
 
+sqlite3 singo.db 'delete from phrases where locale="it_IT" and Themes="finance"'
+sqlite3 singo.db 'insert into phrases (Phrase, Locale,Themes) select distinct Keyword as Phrase,"it_IT" as Locale,"finance" as Themes from it_IT_finance_phrases'
+
+
+
 sqlite3 -init backup.sql singo.db
 
 GOBIN=./bin GOPATH=$PWD:/home/juno/junoworkspace/gows go install -a src/app.go
