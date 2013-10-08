e6d2b59f86715456d9ac55fc49c2c11880db23c7
Sinelga <support@sinelga.com>
2013-10-08 04:48:59 +0300

    prod

--- goFastCgi/usefull.bash
+++ goFastCgi/usefull.bash
@@ -5,6 +5,17 @@ cp  singo.db  singo.db.last
 rm singo.db-journal singo.db
 echo .dump | sqlite3 singo.db >backup.sql
 
+## Backup
+echo '.dump' | sqlite3 singo.db | gzip -c >singo.dump.gz
+
+sqlite3 singo.db 'select * from keywords where locale="it_IT" and Themes="finance"' 
+
+sqlite3 singo.db 'select Locale,Themes,Keyword from it_IT_finance_keywords where Block=0'
+
+sqlite3 singo.db 'delete from keywords where locale="it_IT" and Themes="finance"'
+
+sqlite3 singo.db 'insert into keywords ( Locale,Themes,Keyword) select Locale,Themes,Keyword from it_IT_finance_keywords where Block=0'
+
 sqlite3 -init backup.sql singo.db
 
 GOBIN=./bin GOPATH=$PWD:/home/juno/junoworkspace/gows go install -a src/app.go
