325224fbc732e5697044b1302617637e544ddb7b
Sinelga <support@sinelga.com>
2013-10-13 06:32:10 +0300

    prod

--- goFastCgi/markresources/fi_FI_fortune/usefull.bash
+++ goFastCgi/markresources/fi_FI_fortune/usefull.bash
@@ -1,6 +1,6 @@
  #!/bin/bash
   
-split -d -l 110  all_fi_FI_fortune.txt fi_FI_fortune
+split -d -l 500  all_fi_FI_fortune.txt fi_FI_fortune
 
 #split -d -b 455K all_fi_FI_fortune.txt fi_FI_fortune
  
