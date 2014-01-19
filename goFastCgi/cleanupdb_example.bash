7aa3fde99483a091c4fd1ae1c5f286fc3334ba4b
Sinelga <support@sinelga.com>
2014-01-19 05:41:23 +0200

    p

--- goFastCgi/cleanupdb_example.bash
+++ goFastCgi/cleanupdb_example.bash
@@ -6,3 +6,8 @@ sqlite3 singo.db 'delete from keywords where locale="fi_FI" and Themes="porno"'
 sqlite3 singo.db 'delete from phrases where locale="fi_FI" and Themes="porno"'
 sqlite3 singo.db 'delete from keywords where locale="fi_FI" and Themes="fortune"'
 sqlite3 singo.db 'delete from phrases where locale="fi_FI" and Themes="fortune"'
+
+
+rm -fr markresources/fi_FI_finance/
+rm -fr markresources/fi_FI_fortune/
+rm -fr markresources/it_IT_finance/
