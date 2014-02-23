7783cac2ef3b954f1f47abb1a1994ae305ef04bb
Sinelga <support@sinelga.com>
2014-02-23 07:16:03 +0200

    prod

--- goFastCgi/markresources/it_IT_finance/usefull.bash
+++ goFastCgi/markresources/it_IT_finance/usefull.bash
@@ -3,4 +3,8 @@
 split -d -l 2500  all_it_IT_finance.txt it_IT_finance
 
 #split -d -b 455K all_it_IT_finance.txt it_IT_finance
+
+ sed 1d allit_IT_finance.txt >it_IT_finance09
+ 
+ rm allit_IT_finance.txt
  
