ae9acac82d03b382efbb4ae252d023d755a4fb36
Sinelga <support@sinelga.com>
2014-03-12 06:32:53 +0200

    prod

--- goFastCgi/monitcleanup_example.bash
+++ goFastCgi/monitcleanup_example.bash
@@ -3,3 +3,4 @@ cd /home/juno/git/goFastCgi/goFastCgi
 pgrep elabqueue || pgrep orphans || pgrep  cleanupspace || bin/cleanupspace
 pgrep elabqueue || pgrep cleanupspace || pgrep orphans || bin/orphans -updated=800
 /usr/bin/find www -type d -empty -delete
+
